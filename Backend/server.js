105975c0186c306bf7b5bf746c64f6bbe35a0b8d
Nitesh Chavan <chavannitesh963@gmail.com>
2026-09-19 16:22:25 +0530

    Fix in ES module

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 import app from './src/app.js';
 import ConnectDB from './src/config/database.js';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 console.log("google/genai version:", require('@google/genai/package.json').version);
 
 ConnectDB()
